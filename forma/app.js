83d6f6ed503fcb452f72e9636659efd5b8ada510
Ervin Kacar <kacarervin@gmail.com>
2021-07-12 13:44:22 +0200

    Form exercise

--- forma/app.js
+++ forma/app.js
@@ -1,3 +1,18 @@
-const inputs = document.querySelectorAll('.input');
+const userValue = 'admin'
+const passValue = 'admin'
 
-inputs.forEach
+document.querySelector('#login').onsubmit = function (e) {
+    e.preventDefault()
+    varUsername = document.getElementById("user").value;
+    varPassword = document.getElementById("pass").value;
+    if (varUsername === userValue && varPassword === passValue) {
+        document.getElementById("user").value = ''
+        document.getElementById("pass").value = ''
+        location.replace('home.html')
+    } else {
+        document.getElementById("user").value = ''
+        document.getElementById("pass").value = ''
+        alert('Neispravna sifra')
+    }
+}
+
