e417de80ce6e11701e495269fc27d0a98a7b6f32
Ervin Kacar <kacarervin@gmail.com>
2021-09-08 13:38:29 +0200

    async settimeout example

--- Zadaci Javascript/sincasyncfunc/vezba.js
+++ Zadaci Javascript/sincasyncfunc/vezba.js
@@ -1,7 +1,7 @@
 
 //Sinc code example
 
-function otherFunct(){
+ /* function otherFunct(){
     console.log('We are another function');
     console.log('Do some stuff');
 }
@@ -10,5 +10,22 @@ console.log('start');
 
 otherFunct();
 
+
+console.log('end');   */
+
+
+//Async code xample 
+
+console.log('start'); 
+
+setTimeout(()=>{
+console.log('we are in the timeouc');
+},5000)
+
+const items = [1,2,3,4,5];
+
+items.forEach(item=>{
+console.log(item);
+});
 
 console.log('end'); 
