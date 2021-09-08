7b7c1f755702471e0f351ab11540f189631a6519
Ervin Kacar <kacarervin@gmail.com>
2021-09-08 16:58:50 +0200

    new example with promises

--- Zadaci Javascript/sincasyncfunc/promises.js
+++ Zadaci Javascript/sincasyncfunc/promises.js
@@ -11,7 +11,7 @@ promise.then(user =>{
     
     // promises with reject
 
-const promise = new Promise ((resolve, reject)=>{
+   /*  const promise = new Promise ((resolve, reject)=>{
     setTimeout(()=>{
         console.log("got the user");
         reject(new Error("User not logged in"));
@@ -20,4 +20,46 @@ const promise = new Promise ((resolve, reject)=>{
 
 promise.then(user =>{
     console.log(user);
-}).catch(err => console.log(err.message));
+}).catch(err => console.log(err.message));   */
+
+
+console.log("start");
+
+
+function loginUser(email,password, callback){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("Now we have the data");
+            resolve ({userEmail: email});
+        },3000);
+    });
+} 
+   
+function getUserVideos(email,callback){
+    return new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+  resolve (["video1", "video2", "video3"]);
+  },2000);
+});
+}
+function videoDetails(video){
+    return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+  resolve ("title of a video");
+    },2000);
+    });
+  }
+
+//const user = loginUser ("kacarervin@gmail.com", 123445, (user)=>{
+//console.log(user);
+//getUserVideos(user.userEmail, videos => {
+//console.log(videos);
+//});
+//});
+
+loginUser("ed", "bumba")
+.then(user => getUserVideos(user.email))
+.then(videos => videoDetails(videos[0]))
+.then(detail => console.log(detail));
+
+console.log("finish");
