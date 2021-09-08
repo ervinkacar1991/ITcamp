77378632207e2eabc23e702be72838251cd427ad
Ervin Kacar <kacarervin@gmail.com>
2021-09-08 13:50:59 +0200

    new callback example

--- Zadaci Javascript/sincasyncfunc/vezba.js
+++ Zadaci Javascript/sincasyncfunc/vezba.js
@@ -16,7 +16,7 @@ console.log('end');   */
 
 //Async code xample 
 
-console.log('start'); 
+ /* console.log('start'); 
 
 setTimeout(()=>{
 console.log('we are in the timeouc');
@@ -28,4 +28,23 @@ items.forEach(item=>{
 console.log(item);
 });
 
-console.log('end'); 
+console.log('end');   */
+
+
+
+console.log("start");
+
+
+function loginUser(email,password, callback){
+    setTimeout(()=>{
+        console.log("Now we have the data");
+        callback ({userEmail: email});
+    },5000)
+}
+
+const user = loginUser ("kacarervin@gmail.com", 123445, (user)=>{
+console.log(user);
+});
+
+
+console.log("finish");
