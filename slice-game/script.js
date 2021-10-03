c1460847683da6721b29932727d6ea538b139809
Ervin Kacar <kacarervin@gmail.com>
2021-10-03 23:23:01 +0200

    puzzle game in js, finished

--- slice-game/script.js
+++ slice-game/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', shuffleAll);
 
 let allLevelDivs = document.querySelectorAll('[class*="level"]');
+let twoSlices = [];
+let currentView = 'level-1';
 
 function shuffleAll() {
    
@@ -23,6 +25,60 @@ function shuffleAll() {
 function startGame(){
     // set links to show hide
     setNavLinks();
+    //set clicks on all slices
+    addClicksToSlices();
+}
+
+function addClicksToSlices (){
+    let allSlices = document.querySelectorAll('[class*="img-holder"]');
+    for (const slice of allSlices) {
+        slice.addEventListener('click', selectMe)
+    }
+}
+
+function selectMe () {
+    this.style.border = "2px solid green";
+    twoSlices.push(this);
+    if(twoSlices.length === 2){
+       //get order of clicks
+       let orderFirst = window.getComputedStyle(twoSlices[0]).getPropertyValue('order');
+       let orderSecond = window.getComputedStyle(twoSlices[1]).getPropertyValue('order');
+       //reorder zamena mesta
+       twoSlices[0].style.order = orderSecond;
+       twoSlices[1].style.order = orderFirst;
+
+       twoSlices[0].style.border = 'none';
+       twoSlices[1].style.border = 'none';
+
+       // reset
+
+       twoSlices.length = 0;
+
+       checkIsComplete();
+
+    }
+}
+
+function checkIsComplete(){
+    let currentDiv = document.querySelector('.'+currentView);
+   let allSlices = currentDiv.querySelectorAll('[class*="img-holder"]');
+   let correctOrder = [];
+   for (let i = 0; i < allSlices.length; i++) {
+       correctOrder.push(i+1)
+       
+   }
+   let currentOrder = [];
+   for (let i = 0; i < allSlices.length; i++) {
+       const slice = allSlices[i];
+       currentOrder.push(window.getComputedStyle(slice).getPropertyValue('order'));
+       
+   }
+   if(currentOrder.toString() == correctOrder.toString()){
+       let activeLink = document.querySelector('.active');
+       activeLink.classList.add('finished');
+       currentDiv.style.border = '5px solid green';
+       currentDiv.style.boxShadow= '0 0 16px green'
+       }
 }
 
 function setNavLinks(){
@@ -30,7 +86,12 @@ function setNavLinks(){
     for (let i = 0; i < headerNavLinks.length; i++) {
         const link = headerNavLinks[i];
         link.addEventListener('click', function(){
-            let currentView = this.getAttribute('data-lvl');
+         currentView = this.getAttribute('data-lvl');
+         for (const myLink of headerNavLinks) {
+             myLink.classList.remove('active');
+             
+         }
+         this.classList.add('active');
             for (let i = 0; i < allLevelDivs.length; i++) {
                 allLevelDivs[i].style.display = 'none'; 
             }
